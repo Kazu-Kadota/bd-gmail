3ed3d5ef9a13b77e52a1c69b6bd5de3fc00555fc
Kazu-Kadota <kadota.kazuyuki@gmail.com>
2023-01-20 18:08:24 -0300

    worker: create array with taken content

--- src/models/message.ts
+++ src/models/message.ts
@@ -22,4 +22,5 @@ export interface MessageContent {
     }
     attachment?: AttachmentContent[]
   }
+  description?: string
 }
--- src/workers/get-gmail-content/message-content-constructor.ts
+++ src/workers/get-gmail-content/message-content-constructor.ts
@@ -19,30 +19,41 @@ const messageContentConstructor = async (
 
   const message = await getMessage(access_token, message_id)
 
-  const from = verifyMessageFrom(message)
+  if (message.labelIds?.find((value) => value === 'CATEGORY_PERSONAL')) {
+    // console.log('Message: Payload')
+    // console.log(message.payload)
 
-  const date = verifyMessageDate(message)
+    const from = verifyMessageFrom(message)
 
-  const text_encoded = getMessageText(message)
+    const date = verifyMessageDate(message)
 
-  const text_decoded = decodeMessageText(text_encoded)
+    const text_encoded = getMessageText(message)
 
-  const message_content_object: MessageContent = {
-    id: message.id as string,
-    snippet: message.snippet as string,
-    payload: {
-      headers: {
-        from,
-        date,
+    // console.log('Message: Text encoded')
+    // console.log(text_encoded)
+
+    const text_decoded = decodeMessageText(text_encoded)
+
+    return {
+      id: message.id as string,
+      snippet: message.snippet as string,
+      payload: {
+        headers: {
+          from,
+          date,
+        },
+        body: {
+          base64: text_encoded,
+          content: text_decoded,
+        },
       },
-      body: {
-        base64: text_encoded,
-        content: text_decoded,
-      },
-    },
+    }
   }
 
-  return message_content_object
+  return {
+    id: message.id as string,
+    description: 'It is not category personal email',
+  }
 }
 
 export default messageContentConstructor
